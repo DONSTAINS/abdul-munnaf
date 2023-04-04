8ec171d212a942ee70e89c94cddceba308ce91a9
DONSTAINS <donstains1@gmail.com>
2023-04-04 18:52:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+let abdul=confirm(`unoda Bday ku gift veynuma KUNJI MUNNAF?
+ yes(ok), no(cancel)`)
+let kitting= `😂apadi ena pudungita gift keyakura😂`
+if(abdul)
+{
+    switch(prompt(` apo 1 la erundu 3 kula edhana number solu: `)){
+        case`1`:
+             alert(` neyarula un alupeyaru solu  mudinja seyathu veykura`)
+             break;
+        case`2`:
+        alert(`jet vangi thara potukoo  ${kitting}`)
+        break;
+        case`3`:
+        alert(`nai pi vangi thara sapudu  ${kitting}`) 
+        break;
+        default:
+            alert(`sonadha matum sei da makata `)
+        }
+} 
+else {
+    alert(`nala pulla. Ne keyatalum tharamata da poramboku`)
+}
+
+
+
+
+
+
+
+    
